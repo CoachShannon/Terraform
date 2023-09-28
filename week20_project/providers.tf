64b645fd2461bb7ce8652ab0c347ba45fb86da05
Shannon Coach <shannoncoach44@gmail.com>
2023-09-28 05:11:21 +0000

    project work

--- week20_project/providers.tf
+++ week20_project/providers.tf
@@ -1,17 +1,29 @@
-# Configure the AWS Provider
-provider "aws" {
-region = "us-east-1"
-}
-
 terraform {
   required_providers {
-    serverscom = {
-      source = "serverscom/serverscom"
-      version = "0.3.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "serverscom" {
-  # Configuration options
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "172.31.0.0/16"
+}
+
+#create s3 bucket
+
+resource "aws_s3_bucket" "shannonLUITbucket2023"
+  bucket = var.bucketname
+
+  tags = {
+    name        = "shannonLUITbucket2023"
+    environment = "week_20_project"
+    }
 }
--- week20_project/variables.tf
+++ week20_project/variables.tf
@@ -3,7 +3,38 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "vpc_name" {
-  type    = string
-  default = ""
+variable "ami_id" {
+  description = "ami id for instance"
+  type        = string
+  default     = "ami-03a6eaae9938c858c"
+}
+
+variable "project_instance" {
+  description = "instance_type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "my_ami_id"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "my_ami_id"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "my_ami_id"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "my_ami_id"
+  type        = string
+  default     = "t2.micro"
 }
