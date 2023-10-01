8de04f15aa337b7a13aa93d7e4e1def7827802ea
Shannon Coach <shannoncoach44@gmail.com>
2023-10-01 03:16:50 +0000

    code repair

--- week20_project/main.tf
+++ week20_project/main.tf
@@ -11,8 +11,6 @@ resource "aws_instance" "jenkins" {
   }
   vpc_security_group_ids = [aws_security_group.week_20_sg.id]
 }
-
-
 #create a security group
 resource "aws_security_group" "week_20_sg" {
   name   = "allow_tls and ssh"
@@ -34,7 +32,6 @@ resource "aws_security_group" "week_20_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -46,5 +43,3 @@ resource "aws_security_group" "week_20_sg" {
     Name = "allow_tls_and_ssh"
   }
 }
-
-
--- week20_project/providers.tf
+++ week20_project/providers.tf
@@ -8,20 +8,17 @@ terraform {
   }
 }
 
-
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
-
 
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "172.31.0.0/16"
 }
 
-
-
+#create s3 bucket
 resource "aws_s3_bucket" "shannonluitbucketredteam" {
   bucket = var.bucketname
 
