54dd75e5ece5c0284eb2627aeea5a3825bc93e51
Shannon Coach <shannoncoach44@gmail.com>
2023-10-09 22:21:23 +0000

    1

---
+++ week21_project/main.tf
@@ -0,0 +1,56 @@
+#--- main/week 21 project ---
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+resource "aws_s3_bucket" "backend-scoach" {
+  bucket = "example-bucket"
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
